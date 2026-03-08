e8dabea40a84f85f80a29d589192d9c15d4468d4
Xumanstha <shresthaangura@gmail.com>
2026-03-08 08:04:24 +0545

    IntersectionObeservers

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header=document.querySelector(".header");
+const tabs=document.querySelectorAll('.operations__tab');
+const tabsContainer=document.querySelector('.operations__tab-container');
+const tabsContent=document.querySelectorAll(".operations__content");
+const nav=document.querySelector(".nav");
 
 const openModal = function (e) {
   e.preventDefault()
@@ -116,9 +120,7 @@ document.querySelector('.nav__links').addEventListener("click",function(e){
   }
 })
 
-const tabs=document.querySelectorAll('.operations__tab');
-const tabsContainer=document.querySelector('.operations__tab-container');
-const tabsContent=document.querySelectorAll(".operations__content");
+
 
 tabsContainer.addEventListener("click",function(e){
   // console.log(e.target.closest('.operations__tab'));
@@ -141,9 +143,6 @@ tabsContainer.addEventListener("click",function(e){
 })
 
 //Menu fade animation
-const nav=document.querySelector(".nav");
-
-
 function handleHover(e,opacity){
   if(e.target.classList.contains('nav__link')){
     const link=e.target;
@@ -171,10 +170,115 @@ nav.addEventListener('mouseout',handleHover.bind(1));
 
 
 
+//Sticky Navigation
+// const initialCoordinates=section1.getBoundingClientRect();
+// // console.log(initialCoordinates);
 
+// window.addEventListener('scroll',function(){
+//   // console.log(this.window.scrollY);
+//   if(this.window.scrollY>initialCoordinates.top){
+//     nav.classList.add('sticky');
+//   }
+//   else{
+//     nav.classList.remove('sticky');
+//   }
+// })
+
+
+//Better sticky navigation: Intersection Observer API
+//  const obsCallback=function(entries,observer){
+//   entries.forEach(entry=>{
+//     console.log(entry);
+//   });
+//  }
+
+//  const obsOptions={
+//   root:null,
+//   threshold:[0,0.2]
+//  };
+
+//  const observer=new IntersectionObserver(obsCallback,obsOptions);
+//  observer.observe(section1);
+
+// const header=document.querySelector('.header');
+const navHeight=nav.getBoundingClientRect().height;
+
+const stickyNav=function(entries){
+  const [entry]=entries;
+  // console.log(entry);
+
+  if(!entry.isIntersecting) nav.classList.add('sticky');
+  else
+    nav.classList.remove('sticky'); 
+}
+
+const headerObserver=new IntersectionObserver(
+  stickyNav,{
+    root:null,
+    threshold:0,
+    rootMargin:`-${navHeight}px`,
+  }
+);
+
+headerObserver.observe(header);
+
+
+
+//Reveal Sections
+const allSections=document.querySelectorAll('.section');
+const revealSection=function(entries,observer){
+  // const [entry]=entries;
+  // console.log(entry);
+  entries.forEach(entry=>{
+    if(!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target); 
+  }); 
+}
+
+const sectionObserver=new IntersectionObserver(revealSection,{
+root:null,
+threshold:0.15,
+});
+
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
+
+
+const imgTargets=document.querySelectorAll('img[data-src]');
+
+const loadImg=function(entries,observer){
+  const [entry]=entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+
+  //replace src with data-src
+  entry.target.src=entry.target.dataset.src;
+
+  entry.target.addEventListener('load',function(){
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+}
+
+const imgObserver=new IntersectionObserver(loadImg,{
+  root:null,
+  threshold:0,
+  rootMargin:'200px',
+})
+
+imgTargets.forEach(img=>imgObserver.observe(img));
+
+
+//Slider
+const slides=document.querySelectorAll('.slide');
 
 //DOM traversing
-const h1=document.querySelector('h1');
+// const h1=document.querySelector('h1');
 
 //Going downwards: child
 // console.log(h1.querySelectorAll('.highlight'));
